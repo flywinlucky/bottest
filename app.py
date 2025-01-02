c9ba264ac82390cd785a780caba2a6d8d0cd83af
Traffic Wolf <flywinlucky777@gmail.com>
2025-01-02 22:25:03 +0200

    secretbottoken

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ from dotenv import load_dotenv
 # Încarcă variabilele din fișierul .env
 load_dotenv()
 
-# Token-ul botului
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+# Verifică dacă este pe server (dacă RENDER_APP_DOMAIN este setat)
+if os.getenv("RENDER_APP_DOMAIN"):
+    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Token-ul din variabilele de mediu
+else:
+    # Citeste token-ul din fisierul secretbottoken.txt
+    with open("secretbottoken.txt", "r") as file:
+        TOKEN = file.read().strip()
 
 # Inițializează bot-ul cu threaded=False
 bot = telebot.TeleBot(TOKEN, threaded=False)
@@ -16,9 +21,9 @@ bot = telebot.TeleBot(TOKEN, threaded=False)
 app = Flask(__name__)
 
 # Comanda /start
-@bot.message_handler(commands=['start', 'Start'])
+@bot.message_handler(commands=['start', 'DA'])
 def send_welcome(message):
-    bot.reply_to(message, "Salut! Sunt un bot simplu și sunt aici să te ajut.")
+    bot.reply_to(message, "Salut!")
 
 # Endpoint pentru webhook
 @app.route('/' + TOKEN, methods=['POST'])
@@ -28,12 +33,17 @@ def webhook():
     bot.process_new_updates([update])
     return "OK", 200
 
-# Setează webhook-ul
+# Setează webhook-ul sau rulează local
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    bot.remove_webhook()
-    bot.set_webhook(url=f"https://{os.getenv('RENDER_APP_DOMAIN')}/{TOKEN}")
-    return "Bot is running!", 200
+    if os.getenv("RENDER_APP_DOMAIN"):
+        bot.remove_webhook()
+        bot.set_webhook(url=f"https://{os.getenv('RENDER_APP_DOMAIN')}/{TOKEN}")
+        return "Webhook set!", 200
+    else:
+        return "Bot is running in local mode! Webhook not set.", 200
 
 if __name__ == "__main__":
+    bot.polling(none_stop=True)
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
